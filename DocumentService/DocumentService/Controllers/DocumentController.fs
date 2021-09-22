4787e8391fee13f4bd74e37b29579a3b3dd6b04e
Ravil <graf.rav@gmail.com>
2021-09-22 18:37:04 +0300

    init project, create

---
+++ DocumentService/DocumentService/Controllers/DocumentController.fs
@@ -0,0 +1,42 @@
+﻿namespace DocumentService
+
+open System
+open System.IO
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+[<ApiController>]
+[<Route("[controller]")>]
+type DocumentController(logger: ILogger<DocumentController>) =
+    inherit ControllerBase()
+
+    let summaries =
+        [| "Freezing"
+           "Bracing"
+           "Chilly"
+           "Cool"
+           "Mild"
+           "Warm"
+           "Balmy"
+           "Hot"
+           "Sweltering"
+           "Scorching" |]
+
+    let dir = "./data/files"
+        
+    [<HttpPost>]
+    member _.Create(name: string, file: IFormFile) =
+        let filePath = Path.Combine(dir, name)
+
+        
+        let pathExists () = Directory.Exists(dir)
+        let createDir () = 
+            if not (pathExists ()) then
+                Directory.CreateDirectory(dir) |> ignore
+        
+        let saveFile (fs : FileStream) =
+            file.CopyTo(fs);
+
+        createDir ()
+        using (new FileStream(filePath, FileMode.Create)) saveFile
