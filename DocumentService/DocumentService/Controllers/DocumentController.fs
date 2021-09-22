7e660f485327bca8404c456434405e9cae8cdbe6
Ravil <graf.rav@gmail.com>
2021-09-22 19:35:34 +0300

    get by name

--- DocumentService/DocumentService/Controllers/DocumentController.fs
+++ DocumentService/DocumentService/Controllers/DocumentController.fs
@@ -2,9 +2,13 @@
 
 open System
 open System.IO
+open System.Net
+open System.Net.Http
 open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.Mvc
+open Microsoft.AspNetCore.StaticFiles
 open Microsoft.Extensions.Logging
+open Microsoft.Net.Http.Headers
 
 [<ApiController>]
 [<Route("[controller]")>]
@@ -24,19 +28,34 @@ type DocumentController(logger: ILogger<DocumentController>) =
            "Scorching" |]
 
     let dir = "./data/files"
-        
+
     [<HttpPost>]
     member _.Create(name: string, file: IFormFile) =
         let filePath = Path.Combine(dir, name)
 
-        
+
         let pathExists () = Directory.Exists(dir)
-        let createDir () = 
+
+        let createDir () =
             if not (pathExists ()) then
                 Directory.CreateDirectory(dir) |> ignore
-        
-        let saveFile (fs : FileStream) =
-            file.CopyTo(fs);
+
+        let saveFile (fs: FileStream) = file.CopyTo(fs)
 
         createDir ()
         using (new FileStream(filePath, FileMode.Create)) saveFile
+
+    [<HttpGet>]
+    member _.Get(name: string) =
+        let filePath = Path.Combine(dir, name)
+        
+        let provider = FileExtensionContentTypeProvider();
+        
+        let content = provider.TryGetContentType(filePath)
+        let mutable gotContentType, contentType = content
+        if not gotContentType
+             then
+                 contentType = "application/octet-stream" |> ignore
+        
+        let bytes = File.ReadAllBytes(filePath);
+        FileContentResult(bytes, contentType);
